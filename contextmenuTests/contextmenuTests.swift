31d367f6da14114cf66d305325eee23a39935e71
AndriiS-DevBrother <andrii.s@devbrother.com>
2025-05-08 16:13:19 +0300

    init

---
+++ contextmenuTests/contextmenuTests.swift
@@ -0,0 +1,17 @@
+//
+//  contextmenuTests.swift
+//  contextmenuTests
+//
+//  Created by Andrii Stefaniv on 08.05.2025.
+//
+
+import Testing
+@testable import contextmenu
+
+struct contextmenuTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
