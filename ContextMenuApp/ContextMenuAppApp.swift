908e7571bc81ad785a2420017195917e5dc8dd4a
AndriiS-DevBrother <andrii.s@devbrother.com>
2025-05-15 20:32:16 +0300

    Add initial implementation of ContextMenuApp with SwiftUI
    - Introduced `ContentView.swift` as the main view for the application, displaying a simple greeting.
    - Created `ContextMenuAppApp.swift` to serve as the entry point for the SwiftUI application.
    - Added entitlements file `ContextMenuApp.entitlements` to enable app sandboxing and network client permissions.
    - Included asset catalog files for app icons and accent colors.
    - Updated `generate-proto.sh` to include comments for clarity on gRPC plugin usage.
    - Modified Xcode project settings to integrate the new app structure and ensure proper build configurations.

---
+++ ContextMenuApp/ContextMenuAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContextMenuAppApp.swift
+//  ContextMenuApp
+//
+//  Created by Andrii Stefaniv on 15.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct ContextMenuAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
