2745489b7ebca34a79e60d9e2dbd48f92e9011b1
AndriiS-DevBrother <andrii.s@devbrother.com>
2025-05-15 20:41:15 +0300

    Revert "Remove unused files and project components"
    This reverts commit 378ac8bcc99be80748fce0fc6bba4e7fc901b287.

---
+++ contextmenu/contextmenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  contextmenuApp.swift
+//  contextmenu
+//
+//  Created by Andrii Stefaniv on 08.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct contextmenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
